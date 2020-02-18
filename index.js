fc874aecda25dcaa65284e2d0e5f0253ad3ebdf1
tonezone108 <tonezone108@gmail.com>
2020-02-17 19:06:37 -0600

    Added routers and controllers for incomeAllocation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const usersRouter = require("./routers/users");
 const authRouter = require("./routers/auth");
 const incomeExpensesRouter = require("./routers/incomeExpenses");
+const incomeAllocationRouter = require("./routers/incomeAllocation");
 const { logger } = require("./middleware");
 const app = express();
 const port = process.env.PORT || 4001;
@@ -12,6 +13,7 @@ app.use(logger);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/entry", incomeExpensesRouter);
+app.use("/allocation", incomeAllocationRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to our server!");
